0fa8efa28d2b94f38efdb10bf8f0db57f8ec1fef
SachaAttiach <settiach@hotmail.co.uk>
2022-02-08 16:58:47 +0200

    backend complete

--- server/index.js
+++ server/index.js
@@ -43,6 +43,32 @@ app.get("/todos/:id", async (req, res) => {
   }
 });
 
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("todo was updated");
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("todo was deleted");
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
