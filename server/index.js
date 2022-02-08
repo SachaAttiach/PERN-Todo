8f39033e72f8f3768eabb61159e5034bfb3c2b5d
SachaAttiach <settiach@hotmail.co.uk>
2022-02-08 16:25:22 +0200

    routes started

--- server/index.js
+++ server/index.js
@@ -1,5 +1,47 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const dotenv = require("dotenv");
+//middleware
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+//routes
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
